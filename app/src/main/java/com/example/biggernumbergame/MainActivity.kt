d5e34824822d82eb7097b487b7e78e88421e1718
MSJSurprise <mesanwoolasurprise303@gmail.com>
2022-01-14 02:26:26 +0100

    Added the background color change. The color changes based on what the user select. When the user select the the bigger number it change to Green otherwise Red.

--- app/src/main/java/com/example/biggernumbergame/MainActivity.kt
+++ app/src/main/java/com/example/biggernumbergame/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.biggernumbergame
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,16 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        addRandomNum()
         val btnLeft: Button = findViewById(R.id.btn_left)
         val btnRight: Button = findViewById(R.id.btn_right)
-        btnRight.text = Random().nextInt(10).toString()
-        btnLeft.text = Random().nextInt(10).toString()
-
-        while (btnLeft.text.toString().toInt() == btnRight.text.toString().toInt()) {
-            btnRight.text = Random().nextInt(10).toString()
-            btnLeft.text = Random().nextInt(10).toString()
-        }
-
         btnLeft.setOnClickListener {
             checkNum("left") // true
         }
@@ -29,29 +24,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-private fun checkNum(name: String? = null) {
-    val btnRight: Button = findViewById(R.id.btn_right)
-    val btnLeft: Button = findViewById(R.id.btn_left)
-
-    if (name == "left") {
-        if (btnLeft.text.toString().toInt() > btnRight.text.toString().toInt())
-            Toast.makeText(this, "You're Right", Toast.LENGTH_SHORT).show()
-        else
-            Toast.makeText(this, "You're Wrong", Toast.LENGTH_SHORT).show()
-    } else /*if (name == "Right") */ {
-        if (btnRight.text.toString().toInt() > btnLeft.text.toString().toInt())
-            Toast.makeText(this, "You're Right again", Toast.LENGTH_SHORT).show()
-        else
-            Toast.makeText(this, "You're Wrong again", Toast.LENGTH_SHORT).show()
-    }
-
-    btnLeft.text = Random().nextInt(10).toString()
-    btnRight . text = Random ().nextInt(10).toString()
-
-    while (btnLeft.text.toString().toInt() == btnRight.text.toString().toInt()) {
-        btnLeft.text = Random().nextInt(10).toString()
-        btnRight.text = Random().nextInt(10).toString()            }
+    private fun addRandomNum() {
+        val r = Random()
+        val leftbtn = r.nextInt(10)
+        var rightbtn = leftbtn
+        while (leftbtn == rightbtn) {
+            rightbtn = r.nextInt(10)
         }
 
+        val btnLeft: Button = findViewById(R.id.btn_left)
+        val btnRight: Button = findViewById(R.id.btn_right)
+        btnLeft.text = leftbtn.toString()
+        btnRight.text = rightbtn.toString()
+
+    }
+
+    private fun checkNum(name: String? = null) {
+        val btnRight: Button = findViewById(R.id.btn_right)
+        val btnLeft: Button = findViewById(R.id.btn_left)
+        val backgroundView: ConstraintLayout = findViewById(R.id.backgroundView)
+
+        if (name == "left") {
+            if (btnLeft.text.toString().toInt() > btnRight.text.toString().toInt()) {
+                backgroundView.setBackgroundColor(Color.GREEN)
+                Toast.makeText(this, "You're Right", Toast.LENGTH_SHORT).show()
+            }else {
+                backgroundView.setBackgroundColor(Color.RED)
+                Toast.makeText(this, "You're Wrong", Toast.LENGTH_SHORT).show()}
+        } else /*if (name == "Right") */ {
+            if (btnRight.text.toString().toInt() > btnLeft.text.toString().toInt()) {
+                backgroundView.setBackgroundColor(Color.GREEN)
+                Toast.makeText(this, "You're Right again", Toast.LENGTH_SHORT).show()
+            }else
+                backgroundView.setBackgroundColor(Color.RED)
+                Toast.makeText(this, "You're Wrong again", Toast.LENGTH_SHORT).show()
+        }
+
+        addRandomNum()
+    }
 }
